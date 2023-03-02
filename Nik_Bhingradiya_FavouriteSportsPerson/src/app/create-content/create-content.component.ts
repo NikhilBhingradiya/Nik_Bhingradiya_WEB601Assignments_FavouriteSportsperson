f1395f8659ea675839323559a343b455f2eaadac
NikhilBhingradiya <NB76@myscc.ca>
2023-03-02 15:54:23 -0500

    update create content

--- Nik_Bhingradiya_FavouriteSportsPerson/src/app/create-content/create-content.component.ts
+++ Nik_Bhingradiya_FavouriteSportsPerson/src/app/create-content/create-content.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-create-content',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class CreateContentComponent {
 
+  @Output() newContentAdd = new EventEmitter<Content>();
+  newCreateContent: Content = {
+    id: 0,
+    title:"",
+    name: "",
+    imgURL :"",
+    description:"" ,
+    role:"",
+    gender: ""
+  
+  };
+
+  addCreateContent() {
+    const clonedContent = Object.assign({}, this.newCreateContent);
+    clonedContent.id = Math.floor(Math.random() * 1000);
+    // clonedContent.tags = this.newContent.tags.split(',');
+    this.newContentAdd.emit(clonedContent);
+    this.newCreateContent = {
+      id: 0,
+      title:"",
+      name: "",
+      imgURL :"",
+      description:"" ,
+      role:"",
+      gender: ""
+    };
+  }
 }
